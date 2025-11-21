28a4374c3caf37650e84f29e3973d1937bc51fb3
Nikita Doskov <libfter@gmail.com>
2025-11-21 13:04:47 +0700

    added sort.c

---
+++ sort.c
@@ -0,0 +1,12 @@
+
+void sort(int *list, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (list[j] < list[j + 1]) {
+                int tmp = list[j];
+                list[j] = list[j + 1];
+                list[j + 1] = tmp;
+            }
+        }
+    }
+}
